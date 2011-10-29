d89364e543af9708b3c5fc748b62a7af9b190353
Rune Madsen <rune@runemadsen.com>
2011-10-29 15:49:53 -0400

    Added githubauth

--- application.rb
+++ application.rb
@@ -1,17 +1,18 @@
 require 'bundler'
 Bundler.require
+require './github_auth'
+
+enable :sessions
 
 def github_client
-  OAuth2::Client.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', :authorize_url => 'https://github.com/login/oauth/authorize', :token_url => 'https://github.com/login/oauth/access_token')
+  client = GithubAuth.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2')
 end
 
 get '/' do
-  redirect github_client.auth_code.authorize_url(:redirect_uri => 'http://githunch.heroku.com/oauth')
+  redirect github_client.authorize_url
 end
 
 get '/oauth' do
-  access_token = github_client.auth_code.get_token.get_access_token(params[:code], :redirect_uri => 'http://githunch.heroku.com/oauth')  
-  session[:access_token] = access_token.token
-    # @message = JSON.parse(access_token.get('/api/v1/data.json'))
+  session[:access_token] = github_client.token(params[:code])
   "Succes: #{access_token}"
 end
