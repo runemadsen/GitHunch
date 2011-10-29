547909868c686ec9ce7104a3091e7167f1e5b9c0
Rune Madsen <rune@runemadsen.com>
2011-10-29 16:19:34 -0400

    Everything working, yeah

--- application.rb
+++ application.rb
@@ -4,19 +4,25 @@ require './github_auth'
 
 enable :sessions
 
+# put stuff into helpers
+
 def github_client
   client = GithubAuth.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', :authorize_url => 'https://github.com/login/oauth/authorize', :token_url => 'https://github.com/login/oauth/access_token')
 end
 
+def require_user
+  unless session[:access_token]
+    redirect github_client.authorize_url
+  end
+end
+
 get '/' do
-  redirect github_client.authorize_url
+  require_user
+  
+  "#{session[:access_token]}"
 end
 
 get '/oauth' do
   session[:access_token] = github_client.token(params[:code])
-  redirect '/me'
-end
-
-get '/me' do
-  "#{session[:access_token]}"
+  redirect '/'
 end
