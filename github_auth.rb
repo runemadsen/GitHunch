2901983c8624e227e241c32915989ae3e1249b41
Rune Madsen <rune@runemadsen.com>
2011-10-29 15:58:48 -0400

    more oauth

--- github_auth.rb
+++ github_auth.rb
@@ -1,11 +1,11 @@
 class GithubAuth
   
   def initialize(client_id, client_secret, options)
-    @client = OAuth2::Client.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', :authorize_url => 'https://github.com/login/oauth/authorize', :token_url => 'https://github.com/login/oauth/access_token')
+    @client = OAuth2::Client.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', options)
   end
   
   def authorize_url
-    @client.authorize_url(:redirect_uri => 'http://githunch.heroku.com/oauth')
+    @client.auth_code.authorize_url
   end
   
   def token(code)
