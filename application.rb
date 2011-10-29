957f397d0e9874a8e74302a0159dca67b6f74f8a
Rune Madsen <rune@runemadsen.com>
2011-10-29 15:54:42 -0400

    Changed environment

--- application.rb
+++ application.rb
@@ -5,7 +5,7 @@ require './github_auth'
 enable :sessions
 
 def github_client
-  client = GithubAuth.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2')
+  client = GithubAuth.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', :authorize_url => 'https://github.com/login/oauth/authorize', :token_url => 'https://github.com/login/oauth/access_token')
 end
 
 get '/' do
@@ -14,5 +14,5 @@ end
 
 get '/oauth' do
   session[:access_token] = github_client.token(params[:code])
-  "Succes: #{access_token}"
+  "Succes: #{session[:access_token]}"
 end
--- github_auth.rb
+++ github_auth.rb
@@ -4,8 +4,8 @@ class GithubAuth
     @client = OAuth2::Client.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', options)
   end
   
-  def authorize_url(options)
-    @client.authorize_url(options)
+  def authorize_url
+    @client.authorize_url(:redirect_uri => 'http://githunch.heroku.com/oauth')
   end
   
   def token(code)
