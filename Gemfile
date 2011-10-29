f245b1d9a421b14055483b240f24de97e095aa33
Rune Madsen <rune@runemadsen.com>
2011-10-29 15:32:47 -0400

    Added oauth2

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 gem 'sinatra'
 gem 'json'
+gem 'oauth2'
+gem 'httparty'
--- application.rb
+++ application.rb
@@ -1,6 +1,17 @@
 require 'bundler'
 Bundler.require
 
+def github_client
+  OAuth2::Client.new('ea0fe732615aaa5329f8', 'c872fc1aa1927d1b1635f12e072ef02e14c9e1d2', :authorize_url => 'https://github.com/login/oauth/authorize', :token_url => 'https://github.com/login/oauth/access_token')
+end
+
 get '/' do
- erb :index
+  redirect github_client.auth_code.authorize_url(:redirect_uri => 'http://githunch.heroku.com/oauth')
+end
+
+get '/oauth' do
+  access_token = github_client.auth_code.get_token.get_access_token(params[:code], :redirect_uri => 'http://githunch.heroku.com/oauth')  
+  session[:access_token] = access_token.token
+    # @message = JSON.parse(access_token.get('/api/v1/data.json'))
+  "Succes: #{access_token}"
 end
